4cf315ea2bf995aa20c3b1d8fb0e0344c463112c
Eskinder <eske17.ea@gmail.com>
2023-05-16 10:14:49 +0300

    add member_type

--- Announcement/models.py
+++ Announcement/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 
 class Announcement(models.Model):
+    MEMBER_TYPE_CHOICES = (
+        ('Public', 'Public'),
+        ('Member', 'Member'),
+        ('User', 'User'),
+    )
+    member_type = models.CharField(max_length=10, choices=MEMBER_TYPE_CHOICES)
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     product = models.CharField(max_length=255)
     created_by = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    member_type = models.CharField(max_length=255)
     company_id = models.CharField(max_length=255)
     created_at_position = models.CharField(max_length=255)
