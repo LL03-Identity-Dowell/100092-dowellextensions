e61764d3b6cfbefb38727a668c24a57f7152ed6e
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-05-29 22:01:24 +0000

    Refactor: announcement urls

--- Announcement/urls.py
+++ Announcement/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AnnouncementListCreateView, AnnouncementRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('', AnnouncementListCreateView.as_view()),
-    path('<int:pk>/',
+    path('announcements/', AnnouncementListCreateView.as_view()),
+    path('announcements/<int:pk>/',
          AnnouncementRetrieveUpdateDestroyView.as_view()),
 ]
