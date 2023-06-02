1047e55d0db219d44830c426473346cd67f510aa
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-06-02 05:28:20 +0000

    Updated models

--- notifications/models.py
+++ notifications/models.py
@@ -2,43 +2,40 @@ from django.db import models
 
 # Create your models here.
 
-
-class ProductNotification(models.Model):
-    document_id = models.CharField(max_length=300, blank=True, null=True)
-    document_type = models.CharField(max_length=300, default="product_notification")
-    username = models.CharField(max_length=300)
-    portfolio = models.CharField(max_length=300)
-    product_name = models.CharField(max_length=300)
+class Common(models.Model):
+    created_by = models.CharField(max_length=300)
+    seen = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
     company_id = models.CharField(max_length=300, null=True)
     org_name = models.CharField(max_length=300)
+    org_id = models.CharField(max_length=255)
+
+    class Meta:
+        abstract = True
+
+class ProductNotification(Common):
+    portfolio = models.CharField(max_length=300)
+    product_name = models.CharField(max_length=300)
     title = models.CharField(max_length=500)
     message = models.CharField(max_length=500)
     link = models.CharField(max_length=2048, blank=True, null=True)
     duration = models.CharField(max_length=300, null=True)
-    seen = models.BooleanField(default=False)
     button_status = models.CharField(max_length=300, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.username}'
 
 
-class Notification(models.Model):
-    company_id = models.CharField(max_length=255)
-    org_id = models.CharField(max_length=255)
+class Notification(Common):
     data_type = models.CharField(max_length=255)
     user_type = models.CharField(max_length=255)
     from_field = models.CharField(max_length=255)
     to = models.CharField(max_length=255)
     desc = models.TextField()
     org_name = models.CharField(max_length=255)
-    created_by = models.CharField(max_length=255)
     meant_for = models.CharField(max_length=255)
     flag = models.BooleanField()
     type_of_notification = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    document_id = models.CharField(max_length=300, blank=True, null=True)
-    document_type = models.CharField(max_length=300, default="notification")
 
     def __str__(self):
         return f'{self.org_name}'
