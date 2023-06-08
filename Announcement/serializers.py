b7bd12d5766f7e4563640718246083d3257c7ba5
Eskinder <eske17.ea@gmail.com>
2023-06-08 09:54:03 +0300

    refactored announcement apis

--- Announcement/serializers.py
+++ Announcement/serializers.py
@@ -1,10 +1,69 @@
 from rest_framework import serializers
 from .models import Announcement
+from utils.general import logger
+from datetime import datetime
+from utils.dowell_db_call import (
+    save_document,
+    update_document,
+    ANNOUNCEMENT_COLLECTION
+)
+
+
+# class AnnouncementSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Announcement
+#         fields = ('id', 'description', 'created_at',
+#                   'product', 'created_by', 'is_active', 'member_type', 'company_id', 'created_at_position')
+#         read_only_fields = ('id', 'created_at')
 
 
 class AnnouncementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Announcement
-        fields = ('id', 'description', 'created_at',
-                  'product', 'created_by', 'is_active', 'member_type', 'company_id', 'created_at_position')
-        read_only_fields = ('id', 'created_at')
+        fields = '__all__'
+
+    def create(self, validated_data):
+        try:
+            # Include additional attribute
+            validated_data['created_at'] = datetime.utcnow().isoformat()
+            validated_data['is_Active'] = True
+            # Save data
+            response = save_document(
+                collection=ANNOUNCEMENT_COLLECTION,
+                value=validated_data,
+            )
+            return response
+
+        except Exception as e:
+            logger.info(f"Announcement Not Saved: ({e})")
+            raise ValueError(f"Announcement Not Saved: ({e})")
+
+    def update(self, document: dict, validated_data):
+        try:
+            # Save data
+            response = update_document(
+                collection=ANNOUNCEMENT_COLLECTION,
+                is_Active=True
+                # new_value=validated_data,
+                # document_id=document['document_id']
+            )
+            return response
+
+        except Exception as e:
+            logger.info(f"Announcement Not Saved: ({e})")
+            raise ValueError(f"Announcement Not Saved: ({e})")
+
+    @staticmethod
+    def patch(document_id, document):
+        try:
+            # Save data
+            response = update_document(
+                collection=ANNOUNCEMENT_COLLECTION,
+                new_value=document,
+                document_id=document_id
+            )
+            return response
+
+        except Exception as e:
+            logger.info(f"Announcement Not Saved: ({e})")
+            raise ValueError(f"Announcement Not Saved: ({e})")
--- Announcement/views.py
+++ Announcement/views.py
@@ -2,34 +2,195 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .models import Announcement
 from .serializers import AnnouncementSerializer
+from utils.general import logger
+from rest_framework import status
+from rest_framework.views import APIView
 from utils.dowell_db_call import (
     save_document,
     update_document,
     fetch_document,
     ANNOUNCEMENT_COLLECTION
-    )
+)
+import json
 
 
-class AnnouncementListCreateView(generics.ListCreateAPIView):
-    queryset = Announcement.objects.filter(is_active=True)
-    serializer_class = AnnouncementSerializer
+class AnnouncementList(APIView):
+    """
+        Retreive and Create new announcement list
+    """
+
+    def get(self, request, format=None):
+        try:
+            # Retrieve all records
+            response_json = fetch_document(
+                collection=ANNOUNCEMENT_COLLECTION,
+                fields={
+                    "announcement.is_Active": True
+                },
+            )
+            return Response(response_json)
+
+        except Exception as e:
+            logger.error(f"Bad Request, {str(e)}")
+            Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = AnnouncementSerializer(data=data)
+            if serializer.is_valid():
+                response = serializer.save()
+                return Response(response, status=status.HTTP_201_CREATED)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            logger.error(str(e))
+            return Response({"message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-class AnnouncementRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Announcement.objects.filter(is_active=True)
-    serializer_class = AnnouncementSerializer
+class AnnouncementDetail(APIView):
+    def get(self, request, id, format=None):
+        try:
+            # Retrieve record
+            response_json = fetch_document(
+                collection=ANNOUNCEMENT_COLLECTION,
+                fields={
+                    "_id": id
+                },
+            )
+            return Response(response_json)
 
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        instance.is_active = False
-        instance.save()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+        except Exception as e:
+            logger.error(f"Announcement not found, {str(e)}")
+            Response(
+                {"message": f"Announcement not found, {str(e)}"},
+                status=status.HTTP_404_NOT_FOUND)
 
-    def patch(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    def put(self, request, id, format=None):
+        try:
+            # body = json.loads(request.body)
+            # Retrieve record
+            response = fetch_document(
+                collection=ANNOUNCEMENT_COLLECTION,
+                fields={
+                    "_id": id
+                },
+            )
+
+            if response["isSuccess"] and response['data']:
+                announcement = response["data"][0]['announcement']
+                announcement['is_Active'] = False
+                # announcement['member_type'] = request.body.
+                response = AnnouncementSerializer.patch(
+                    id, announcement)
+
+            else:
+                logger.error(f"Announcement Not Found For {id}")
+                return Response(
+                    {"message": f"Announcement Not Found For {id}"},
+                    status=status.HTTP_404_NOT_FOUND)
+
+            return Response(response, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print(e)
+            logger.error(str(e))
+            return Response({"message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def patch(self, request, id, format=None):
+        try:
+            body = json.loads(request.body)
+            # Retrieve record
+            response = fetch_document(
+                collection=ANNOUNCEMENT_COLLECTION,
+                fields={
+                    "_id": id
+                },
+            )
+
+            if response["isSuccess"] and response['data']:
+                announcement = response["data"][0]['announcement']
+                # Update the announcement object if the corresponding value is present in the body
+                announcement['description'] = body.get(
+                    'description', announcement['description'])
+                announcement['product'] = body.get(
+                    'product', announcement['product'])
+                announcement['member_type'] = body.get(
+                    'member_type', announcement['member_type'])
+                announcement['created_by'] = body.get(
+                    'created_by', announcement['created_by'])
+                announcement['company_id'] = body.get(
+                    'company_id', announcement['company_id'])
+                announcement['created_at_position'] = body.get(
+                    'created_at_position', announcement['created_at_position'])
+
+                # announcement['member_type'] = request.body.
+                response = AnnouncementSerializer.patch(
+                    id, announcement)
+
+            else:
+                logger.error(f"Announcement Not Found For {id}")
+                return Response(
+                    {"message": f"Announcement Not Found For {id}"},
+                    status=status.HTTP_404_NOT_FOUND)
+
+            return Response(response, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print(e)
+            logger.error(str(e))
+            return Response({"message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    # def delete(self, request, document_id, format=None):
+    #     try:
+    #         # Retrieve record
+    #         response = fetch_document(
+    #             collection=ANNOUNCEMENT_COLLECTION,
+    #             fields={
+    #                 "_id": id
+    #             },
+    #         )
+
+    #         if response["isSuccess"] and response['data']:
+    #             announcement = response["data"][0]['announcement']
+    #             announcement['is_Active'] = False
+    #             # Save record
+    #             response = AnnouncementSerializer.patch(
+    #                 announcement)
+
+    #         else:
+    #             logger.error(f"Announcement Not Found For {id}")
+    #             return Response(
+    #                 {"message": f"Announcement Not Found For {id}"},
+    #                 status=status.HTTP_404_NOT_FOUND)
+
+    #         return Response(response, status=status.HTTP_200_OK)
+
+    #     except Exception as e:
+    #         logger.error(str(e))
+    #         return Response({"message": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# class AnnouncementListCreateView(generics.ListCreateAPIView):
+#     queryset = Announcement.objects.filter(is_active=True)
+#     serializer_class = AnnouncementSerializer
+
+
+# class AnnouncementRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Announcement.objects.filter(is_active=True)
+#     serializer_class = AnnouncementSerializer
+
+#     def update(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         instance.is_active = False
+#         instance.save()
+#         serializer = self.get_serializer(instance)
+#         return Response(serializer.data)
+
+#     def patch(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         serializer = self.get_serializer(
+#             instance, data=request.data, partial=True)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
