264e253304cc9e669325f101b9e86fcdb4bccffe
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-16 13:56:26 +0100

    Created the endpoint to delete all announcements

--- healthcheck/urls.py
+++ healthcheck/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from .views import health
+from .views import health, delete_all
 
 
 app_name = "healthcheck"
 urlpatterns = [
     path('', health, name='health'),
     path('health/', health, name='health_1'),
+    path('delete/',delete_all,name="delete")
 ]
