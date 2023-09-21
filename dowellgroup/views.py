1b19c2e238bec550fd503a5a6bb8bdccfa399095
Eskinder <eske17.ea@gmail.com>
2023-09-21 19:45:32 +0300

    add common group name

--- dowellgroup/views.py
+++ dowellgroup/views.py
@@ -33,6 +33,7 @@ class DowellGroupList(APIView):
 
     def post(self, request):
         try:
+
             org_name = request.data.get('org_name')
             org_id = request.data.get('org_id')
             user_id = request.data.get('user_id')
@@ -41,14 +42,15 @@ class DowellGroupList(APIView):
             created_at_position = request.data.get('created_at_position')
             email_list = request.data.get('email_list')
             csv_file = request.data.get('csv_file')
-
+            group_name = request.data.get('group_name')
             data = {
                 'org_name': org_name,
                 'org_id': org_id,
                 'user_id': user_id,
                 'created_by': created_by,
                 'created_at_position': created_at_position,
-                'email_list': email_list
+                'email_list': email_list,
+                'group_name': group_name
             }
 
             if not csv_file:
@@ -82,11 +84,11 @@ class DowellGroupList(APIView):
                             "email": email
                         }
                         group_detail.append(groupData)
+                    data['group_detail'] = group_detail
+                    data['group_name'] = group_detail[0]['group_name']
                 except IndexError:
                     return Response({'error': 'Invalid data format in CSV file.'}, status=status.HTTP_400_BAD_REQUEST)
 
-                data['group_detail'] = group_detail
-                data['group_name'] = group_detail[0]['group_name']
             serializer = DowellGroupSerializer(data=data)
             if serializer.is_valid():
                 response = serializer.save()
