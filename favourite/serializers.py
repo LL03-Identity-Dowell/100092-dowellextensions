ae48ef890c73e77ab09484b2995f4849075654e5
Worlako Dzokoto <worlaco.dzo@gmail.com>
2023-09-29 10:26:32 +0000

    Feat: add favorite images

--- favourite/serializers.py
+++ favourite/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from .models import *
-
+from utils.general import logger
+from datetime import datetime
+from utils.dowell_db_call import (
+    save_document,
+    update_document,
+    FAVORITE_COLLECTION
+)
 
 class favouriteSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=300)
@@ -13,14 +19,37 @@ class favouriteSerializer(serializers.Serializer):
 
 
 class ImageSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
     session_id = serializers.CharField(max_length=300)
+    user_id = serializers.CharField(max_length=50)
     username = serializers.CharField(max_length=300)
-    image = serializers.CharField()
+    image_url = serializers.URLField()
 
-    # image = serializers.ReadOnlyField()
-    # class Meta:
-    #     model = FavouriteImage
-    #     fields = ('id', 'session_id', 'username', 'image')
+    def create(self, validated_data):
+        try:
+            validated_data['created_at'] = datetime.utcnow().isoformat()
+            validated_data['deleted'] = False
+            validated_data['type'] = "favorite-image"
 
-        
+            response = save_document(
+                collection=FAVORITE_COLLECTION,
+                value=validated_data,
+            )
+            return response
+
+        except Exception as e:
+            logger.info(f"Favorite Image Not Saved: ({e})")
+            raise ValueError(f"Favorite Image Not Saved: ({e})")
+
+    @staticmethod
+    def update(document_id, document):
+        try:
+            response = update_document(
+                collection=FAVORITE_COLLECTION,
+                new_value=document,
+                document_id=document_id
+            )
+            return response
+
+        except Exception as e:
+            logger.info(f"Favorite Image Not Saved: ({e})")
+            raise ValueError(f"Favorite Image Not Saved: ({e})")
