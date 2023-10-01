e631ba72793f58a6f7559ae517f7bf0512d202f2
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-01 16:31:32 +0100

    username replaced with user_id

--- favourite/urls.py
+++ favourite/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('favourite/',setasfavourite.as_view()),
     path('favourite/<int:pk>',setasfavourite.as_view()),
-    path('favourites/<str:username>',FavouritesView.as_view()),
+    path('favourites/<str:user_id>',FavouritesView.as_view()),
     path('favouriteIcon/',favouriteIcon.as_view()),
     path('v1/favorites/<str:user_id>/images/', FavouriteImageList.as_view()),
     path('v1/favorites/<str:user_id>/images/<str:favorite_img_id>/', FavouriteImageDetail.as_view())
