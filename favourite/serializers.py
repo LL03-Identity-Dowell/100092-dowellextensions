5f557f2fc73243106e2d2ca8df975f4acd065e9e
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-01 16:34:30 +0100

    Implemented post handler & put handler(commented)

--- favourite/serializers.py
+++ favourite/serializers.py
@@ -9,14 +9,44 @@ from utils.dowell_db_call import (
 )
 
 class favouriteSerializer(serializers.Serializer):
+    user_id = serializers.CharField(max_length=50)
     username = serializers.CharField(max_length=300)
     portfolio = serializers.CharField(max_length=300)
     productName = serializers.CharField(max_length=300)
+    productId = serializers.CharField(max_length=300)
     action = serializers.BooleanField(default= True)
     orgName = serializers.CharField(max_length=300)
     image = serializers.ImageField(required=False)
     image_url = serializers.URLField(required=False)
 
+    # def create(self, validated_data):
+    #     try:
+    #         validated_data['type'] = "favorite"
+    #         validated_data['deleted'] = False
+    #         validated_data['created_at'] = datetime.utcnow().isoformat()
+    #         response = save_document(
+    #             collection=FAVORITE_COLLECTION,
+    #             value=validated_data
+    #         )
+    #         return response
+    #     except Exception as e:
+    #         logger.info(f"Favorite Not Saved: ({e})")
+    #         raise ValueError(f"Favorite Not Saved: ({e})")
+
+    # @staticmethod
+    # def update(document_id, document):
+    #     try:
+    #         response = update_document(
+    #             collection=FAVORITE_COLLECTION,
+    #             new_value=document,
+    #             document_id=document_id
+    #         )
+    #         return response
+
+    #     except Exception as e:
+    #         logger.info(f"Favorite Not Updated: ({e})")
+    #         raise ValueError(f"Favorite Not Updated: ({e})")
+        
 
 class ImageSerializer(serializers.Serializer):
     session_id = serializers.CharField(max_length=300)
