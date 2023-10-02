36b678317b390a333281a21fbc05ad960a607a03
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-02 18:41:42 +0100

    updated endpoints

--- favourite/urls.py
+++ favourite/urls.py
@@ -6,6 +6,12 @@ urlpatterns = [
     path('favourite/<int:pk>',setasfavourite.as_view()),
     path('favourites/<str:user_id>',FavouritesView.as_view()),
     path('favouriteIcon/',favouriteIcon.as_view()),
+
+    #new endpoints
+    path('v1/favorite/',setasfavourite.as_view()),
+    path('v1/favorites/<int:pk>', setasfavourite.as_view()),
+    path('v1/favorites/<str:user_id>',FavouritesView.as_view()),
+    path('v1/favorites/favourite_icon/',favouriteIcon.as_view()),
     path('v1/favorites/<str:user_id>/images/', FavouriteImageList.as_view()),
     path('v1/favorites/<str:user_id>/images/<str:favorite_img_id>/', FavouriteImageDetail.as_view())
 ]
