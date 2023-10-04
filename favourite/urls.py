eb63f66594cb0f15ebd10e381c5e5e57950fef7f
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-04 10:22:24 +0100

    Debuged successfully

--- favourite/urls.py
+++ favourite/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('favouriteIcon/',favouriteIcon.as_view()),
 
     #new endpoints
-    path('v1/favorite/',setasfavourite.as_view()),
-    path('v1/favorites/<int:pk>', setasfavourite.as_view()),
-    path('v1/favorites/<str:user_id>',FavouritesView.as_view()),
+    path('v1/favorites/',setasfavourite.as_view()),
     path('v1/favorites/favourite_icon/',favouriteIcon.as_view()),
+    path('v1/favorites/<str:pk>/', setasfavourite.as_view()),
+    path('v1/users/<str:user_id>/favorites/',FavouritesView.as_view()),
     path('v1/favorites/<str:user_id>/images/', FavouriteImageList.as_view()),
     path('v1/favorites/<str:user_id>/images/<str:favorite_img_id>/', FavouriteImageDetail.as_view())
 ]
