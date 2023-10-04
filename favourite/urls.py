0e2ab0b1bc5a93a26422843cfeedb7a99a43005a
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-04 12:37:39 +0100

    debugged favoriteIcon error

--- favourite/urls.py
+++ favourite/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('favourite/',setasfavourite.as_view()),
     path('favourite/<int:pk>',setasfavourite.as_view()),
     path('favourites/<str:user_id>',FavouritesView.as_view()),
-    path('favouriteIcon/',favouriteIcon.as_view()),
+    # path('favouriteIcon/',favouriteIcon.as_view()),
 
     #new endpoints
     path('v1/favorites/',setasfavourite.as_view()),
